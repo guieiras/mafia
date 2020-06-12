99c9dc4885728998f2695cec032e7c9edd7976c7
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-06-12 14:37:02 -0300

    lint: fix offenses

--- src/components/gameIcon.jsx
+++ src/components/gameIcon.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-export default function GameIcon(props) {
-  const styles = props.style || {};
-  const customClasses = props.className ? ` ${props.className}` : '';
-  const classes = `game-icon game-icon-${props.icon} ${customClasses}`;
+export default function GameIcon({ icon, className, ...props }) {
+  const customClasses = className ? ` ${className}` : '';
+  const classes = `game-icon game-icon-${icon} ${customClasses}`;
 
-  return <i {...props} style={styles} className={classes} />;
+  return <i {...props} className={classes} />;
 }
